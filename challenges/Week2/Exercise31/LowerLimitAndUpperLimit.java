643a04b12dfed15f3941f182a91998f0b20e712a
ragmha <Abul.MidhatHasanRaghib@metropolia.fi>
2017-01-21 04:37:13 +0200

    Add solution Lower limit & Upper limit

---
+++ challenges/Week2/Exercise31/LowerLimitAndUpperLimit.java
@@ -0,0 +1,25 @@
+
+import java.util.Scanner;
+
+public class LowerLimitAndUpperLimit {
+
+    public static void main(String[] args) {
+        Scanner reader = new Scanner(System.in);
+
+        // write your code here
+        System.out.print("First: ");
+        int first = Integer.parseInt(reader.nextLine());
+
+        System.out.print("Last: ");
+        int last = Integer.parseInt(reader.nextLine());
+
+
+        int num = first;
+
+        while (num >= first && num <= last) {
+            System.out.println(num);
+            num++;
+        }
+
+    }
+}
