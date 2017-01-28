5f2de709de2e656ad5fe6b96890a3587c84c9667
ragmha <Abul.MidhatHasanRaghib@metropolia.fi>
2017-01-29 00:54:46 +0200

    Declare firstCharacter method before main

--- challenges/Week3/Exercise48/FirstCharacter.java
+++ challenges/Week3/Exercise48/FirstCharacter.java
@@ -2,6 +2,10 @@ import java.util.Scanner;
 
 public class FirstCharacter {
 
+	public static char firstCharacter(String text) {
+			return text.charAt(0);
+	}
+
     public static void main(String[] args) {
         Scanner reader = new Scanner(System.in);
 
@@ -10,9 +14,5 @@ public class FirstCharacter {
 
         System.out.println("First character: " + firstCharacter(name));
 
-    }
-
-    public static char firstCharacter(String text) {
-        return text.charAt(0);
     }
 }
