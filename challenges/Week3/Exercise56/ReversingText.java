b77b92289bb31fe8a4869b14ab15692f8b56f2b2
ragmha <Abul.MidhatHasanRaghib@metropolia.fi>
2017-01-29 02:23:34 +0200

    Add a simpler ReversingText solution

--- challenges/Week3/Exercise56/ReversingText.java
+++ challenges/Week3/Exercise56/ReversingText.java
@@ -19,3 +19,32 @@ public class ReversingText {
         System.out.println("In reverse order: " + reverse(text));
     }
 }
+
+
+// import java.util.Scanner;
+//
+// public class ReversingText {
+//
+//     public static String reverse(String text) {
+//         // write your code here
+//         // note that method does now print anything, it RETURNS the reversed string
+//         String result = "";
+//
+//         int i = text.length() - 1;
+//
+//         while (i >= 0) {
+//             result += text.charAt(i);
+//             i--;
+//         }
+//
+//         return result;
+//     }
+//
+//     public static void main(String[] args) {
+//         Scanner reader = new Scanner(System.in);
+//         System.out.print("Type in your text: ");
+//         String text = reader.nextLine();
+//         System.out.println("In reverse order: " + reverse(text));
+//     }
+// }
+//
