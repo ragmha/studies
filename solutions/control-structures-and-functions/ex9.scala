16223b28dab8ac060cc3e9e43b12cd3418eda4db
ragmha <Abul.MidhatHasanRaghib@metropolia.fi>
2017-05-26 01:24:45 +0300

    add solution ex9

---
+++ solutions/control-structures-and-functions/ex9.scala
@@ -0,0 +1,10 @@
+/**
+* Exercise 9:
+* Make the function of the preceding exercise a recursive function.
+*
+**/
+def recproduct(s: String): BigInt = {
+  if(s.length == 0) 1 else s.head * recproduct(s.tail)
+}
+
+recproduct("Hello") //=> 9415087488: BigInt
