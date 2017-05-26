09bc9f8ad505eaf230c4e1804497a8dd40ea4c81
ragmha <Abul.MidhatHasanRaghib@metropolia.fi>
2017-05-26 04:35:00 +0300

    add solution ex3

---
+++ solutions/maps-and-tuples/ex3.scala
@@ -0,0 +1,17 @@
+/**
+* Exercise 3:
+*
+* Repeat the preceding exercise with an immutable map.
+*
+**/
+
+val fileName = "test.txt"
+var freq= Map[String, Int]()
+val in = new java.util.Scanner(new java.io.File(fileName)).useDelimiter("\\s")
+
+while(in.hasNext()) {
+  val word = in.next()
+  freq += (word -> (freq.getOrElse(word, 0) + 1 ))
+}
+
+println(freq)
