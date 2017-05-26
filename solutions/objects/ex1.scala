a9309d589c74059ccb1533910ab0ef3991fec080
ragmha <Abul.MidhatHasanRaghib@metropolia.fi>
2017-05-26 05:28:21 +0300

    add solution ex1

---
+++ solutions/objects/ex1.scala
@@ -0,0 +1,17 @@
+/**
+* Exercise 1:
+*
+* Write an object Conversions with methods inchesToCentimeters, gallonsToLiters,
+* and milesToKilometers.
+*
+**/
+
+object Conversions {
+
+  def inchesToCentimeters(inches: Double)= inches * 2.54
+
+  def gallonsToLiters(gallons: Double)= gallons * 3.78541
+
+  def milesToKilometers(miles: Double)= miles * 1.60934
+
+}
