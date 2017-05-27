9d73d1ae31f4df2a047fdca8feb80be69b59a77e
ragmha <Abul.MidhatHasanRaghib@metropolia.fi>
2017-05-27 20:01:39 +0300

    add solution ex3

---
+++ solutions/objects/ex3.scala
@@ -0,0 +1,17 @@
+/**
+* Exercise 3:
+*
+* Define an Origin object that extends java.awt.Point. Why is this not actually
+* a good idea? (Have a close look at the methods of the Point class.)
+*
+**/
+
+object Origin extends java.awt.Point {}
+
+println(Origin) //=> [x=0,y=0]
+
+Origin.move(2, 2)
+
+println(Origin) //=> [x=2,y=2]
+
+// Because Point class has mutation method available called move
