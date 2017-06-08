1c8b145d49aeea39dd2144a03e9e5e1a97ff86ce
ragmha <Abul.MidhatHasanRaghib@metropolia.fi>
2017-06-08 07:32:09 +0300

    update solution

--- solutions/basics/bouncer.js
+++ solutions/basics/bouncer.js
@@ -6,20 +6,8 @@
 
  - Falsy values in JavaScript are false, null, 0, "", undefined, and NaN.
 
- - Remember to use Read-Search-Ask if you get stuck. Write your own code.
-
- - Here are some helpful links:
-
- * Boolean Objects
- * Array.prototype.filter()
 */
 
 const bouncer = arr => arr.filter(Boolean);
 
 module.exports = bouncer;
-
-// TEST
-bouncer([7, "ate", "", false, 9]); // => [7, "ate", 9].
-bouncer(["a", "b", "c"]); // => ["a", "b", "c"].
-bouncer([false, null, 0, NaN, undefined, ""]); // => [].
-bouncer([1, null, NaN, 2, undefined]); // => [1, 2].
