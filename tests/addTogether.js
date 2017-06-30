cdb8cbe686dcfd9f8e9353ee76d33d478caa275d
ragmha <Abul.MidhatHasanRaghib@metropolia.fi>
2017-07-01 01:35:12 +0300

    use es6 syntax

--- tests/addTogether.js
+++ tests/addTogether.js
@@ -1,13 +1,13 @@
 const assert = require("assert");
 const addTogether = require("../lib/addTogether");
 
-describe("#addTogether()", function() {
-  it("should return 5", function() {
+describe("#addTogether", () => {
+  it("should return 5", () => {
     assert.equal(addTogether(2, 3), 5);
     assert.equal(addTogether(2)(3), 5);
   });
 
-  it("should return undefined", function() {
+  it("should return undefined", () => {
     assert.equal(addTogether("http://bitly/IqT6zt"), undefined);
     assert.equal(addTogether(2, "3"), undefined);
     assert.equal(addTogether(2)([3]), undefined);
