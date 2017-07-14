5b6df4080c2631d37bf72ca0207fdee5c43880c4
ragmha <Abul.MidhatHasanRaghib@metropolia.fi>
2017-07-14 19:45:22 +0300

    add solution isUnique with Sets

--- solutions/chapter01/1.1-isUnique/index.js
+++ solutions/chapter01/1.1-isUnique/index.js
@@ -15,4 +15,20 @@ const isUnique = str => {
   return true;
 };
 
+/**
+ * Time: O(n)
+ * Space: O(n)
+ * Uses Set data structure to keep track of seen characters
+ */
 
+const isUniqueSet = str => {
+  let chars = new Set();
+
+  for (let i = 0; i < str.length; i++) {
+    if (chars.has(str[i])) {
+      return false;
+    }
+    chars.add(str[i]);
+  }
+  return true;
+};
