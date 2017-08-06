06c1aa78d1f27438dd1f26db4bb0e72499c90ef8
ragmha <Abul.MidhatHasanRaghib@metropolia.fi>
2017-08-06 16:41:15 +0300

    add solution message between instance and generator :smile:

--- practices/message.js
+++ practices/message.js
@@ -1,9 +1,17 @@
 function* createHello() {
-  yield 'first'; // => generator is paused the 'first' val is returned
+  const word = yield;
+  console.log(word);
 }
 
 // Instance of the createHello generator (you can send and recive to generator)
 const hello = createHello();
 
-console.log(hello.next()); // => { value: 'first', done: false }
-console.log(hello.next()); // => { value: undefined, done: true } End of Generator function
+console.log(hello.next()); // => { value: undefined, done: false }
+
+// Raghib is logged because the generator is resumed!
+console.log(
+  hello.next('Raghib')
+); /* => 
+Raghib 
+{ value: undefined, done: true }
+*/
